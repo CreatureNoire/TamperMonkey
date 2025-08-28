27b3b42e3a6cdb9f6a02da1e9a32c48a76c710fd
CreatureNoire <jasonhousseau@hotmail.fr>
2025-08-28 12:00:58 +0200

    Update mainscript.user.js

--- tampermonkey/mainscript.user.js
+++ tampermonkey/mainscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Script collector auto
 // @namespace    https://github.com/Syfrost/JustWork-Next-Extension
-// @version      1.6.4
+// @version      1.6.5
 // @description  Charge plusieurs scripts distants
 // @author       Cedric G
 // @connect      prod.cloud-collectorplus.mt.sncf.fr
