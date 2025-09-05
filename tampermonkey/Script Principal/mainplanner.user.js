3752a1745faed949fa81ad1d69bb0b317a8c318e
CreatureNoire <jasonhousseau@hotmail.fr>
2025-09-05 10:15:01 +0200

    Remove external script requirement from mainplanner
    Removed the requirement for an external script.

--- tampermonkey/Script Principal/mainplanner.user.js
+++ tampermonkey/Script Principal/mainplanner.user.js
@@ -10,7 +10,7 @@
 // @match        planner.cloud.microsoft/webui/plan/*
 
 
-
+// @require        https://raw.githubusercontent.com/CreatureNoire/TamperMonkey/refs/heads/master/tampermonkey/Extention/Couleur_Planeur
 // @updateURL    https://github.com/CreatureNoire/TamperMonkey/raw/refs/heads/master/tampermonkey/mainscript.user.js
 // @downloadURL  https://github.com/CreatureNoire/TamperMonkey/raw/refs/heads/master/tampermonkey/mainscript.user.js
 // @grant        GM_info
