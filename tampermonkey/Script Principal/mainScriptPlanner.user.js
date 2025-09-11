5406eee345cf7e5472ab320c31d4a3d9ac8e41f2
CreatureNoire <jasonhousseau@hotmail.fr>
2025-09-11 07:24:08 +0200

    Comment out changementSonPlanner.js requirement
    Commented out the requirement for changementSonPlanner.js.

--- tampermonkey/Script Principal/mainScriptPlanner.user.js
+++ tampermonkey/Script Principal/mainScriptPlanner.user.js
@@ -10,6 +10,7 @@
 // @require      https://raw.githubusercontent.com/CreatureNoire/TamperMonkey/refs/heads/master/tampermonkey/Extention/Collector_via_planner
 // @require      https://raw.githubusercontent.com/CreatureNoire/TamperMonkey/refs/heads/master/tampermonkey/Extention/DocMat_via_planner
 // @require      https://raw.githubusercontent.com/CreatureNoire/TamperMonkey/refs/heads/master/tampermonkey/Extention/Couleur_Planeur
+// @require      https://raw.githubusercontent.com/CreatureNoire/TamperMonkey/refs/heads/master/tampermonkey/Extention/changementSonPlanner.js
 // @updateURL    https://github.com/CreatureNoire/TamperMonkey/raw/refs/heads/master/tampermonkey/mainscript.user.js
 // @downloadURL  https://github.com/CreatureNoire/TamperMonkey/raw/refs/heads/master/tampermonkey/mainscript.user.js
 // @grant        GM_info
