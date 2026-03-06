b0f077491ba244a411ba122a2cf9f8515dcea9f1
CreatureNoire <jasonhousseau@hotmail.fr>
2026-03-06 10:05:00 +0100

    Add required scripts for Optimum Tampermonkey script
    Added required scripts for additional functionality.

--- tampermonkey/Script Principal/ScriptOptimum.js
+++ tampermonkey/Script Principal/ScriptOptimum.js
@@ -5,7 +5,8 @@
     // @description  Ajoute quatre boutons RP, RU, CP, RN avec soldes (fin d'année pour RP/RU/CP, aujourd'hui pour RN)
     // @author       Vous
     // @match        https://optimum.sncf.fr/chronotime/*
-    // @require      
+    // @require      https://raw.githubusercontent.com/CreatureNoire/TamperMonkey/refs/heads/master/tampermonkey/Extention/Bouton%20Supl%C3%A9mentaire%20Optimum.js
+    // @require      https://raw.githubusercontent.com/CreatureNoire/TamperMonkey/refs/heads/master/tampermonkey/Extention/CouleurAgenda.js
     // @updateURL    https://raw.githubusercontent.com/CreatureNoire/TamperMonkey/refs/heads/master/tampermonkey/Extention/Bouton%20RP%20et%20RU%20Optimum.js
     // @downloadURL  https://raw.githubusercontent.com/CreatureNoire/TamperMonkey/refs/heads/master/tampermonkey/Extention/Bouton%20RP%20et%20RU%20Optimum.js
     // @grant        none
