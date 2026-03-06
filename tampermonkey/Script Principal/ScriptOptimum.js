751321bd429ad802fb2c0191dcd9906b12f109bb
CreatureNoire <jasonhousseau@hotmail.fr>
2026-03-06 10:01:37 +0100

    Create ScriptOptimum.js for additional buttons
    Adds a new userscript for additional buttons on the Optimum site.

---
+++ tampermonkey/Script Principal/ScriptOptimum.js
@@ -0,0 +1,12 @@
+    // ==UserScript==
+    // @name         Boutons Supplémentaires Optimum
+    // @namespace    http://tampermonkey.net/
+    // @version      1.8
+    // @description  Ajoute quatre boutons RP, RU, CP, RN avec soldes (fin d'année pour RP/RU/CP, aujourd'hui pour RN)
+    // @author       Vous
+    // @match        https://optimum.sncf.fr/chronotime/*
+    // @require      
+    // @updateURL    https://raw.githubusercontent.com/CreatureNoire/TamperMonkey/refs/heads/master/tampermonkey/Extention/Bouton%20RP%20et%20RU%20Optimum.js
+    // @downloadURL  https://raw.githubusercontent.com/CreatureNoire/TamperMonkey/refs/heads/master/tampermonkey/Extention/Bouton%20RP%20et%20RU%20Optimum.js
+    // @grant        none
+    // ==/UserScript==
